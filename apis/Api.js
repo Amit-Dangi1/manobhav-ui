0e31b9f7e4f0e1f3f8ac80b4b6a9e0ac6988965e
Amit-Dangi1 <amitjidangi@gmail.com>
2025-08-31 23:27:55 +0530

    live url

--- apis/Api.js
+++ apis/Api.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://manobhav-emotion-analyzer.onrender.com/";
 export  const API = {
    signup:BASE_URL+"user/create",
    login:BASE_URL+"user/login",
